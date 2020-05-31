21f9a6c0d68ff47ded633be6b8f1c7cd4b1f7d46
Kaushal Wagle <kaushalwagle@gmail.com>
2020-05-31 17:44:05 +0545

    added meal model and dummy data

---
+++ lib/models/meal.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/foundation.dart';
+
+import 'category.dart';
+
+enum Affordability {
+  affordable,
+  expensive,
+  luxurious,
+}
+
+enum Complexity {
+  simple,
+  challenging,
+  difficult,
+}
+
+class Meal {
+  final String id;
+  final List<String> categories;
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final List<String> ingredients;
+  final List<String> steps;
+  final Affordability affordability;
+  final Complexity complexity;
+  final bool isGlutenFree;
+  final bool isVegan;
+  final bool isVegetarian;
+  final bool isLactoseFree;
+
+  const Meal({
+    @required this.id,
+    @required this.categories,
+    @required this.title,
+    @required this.imageUrl,
+    @required this.duration,
+    @required this.steps,
+    @required this.complexity,
+    @required this.affordability,
+    @required this.ingredients,
+    @required this.isGlutenFree,
+    @required this.isVegan,
+    @required this.isVegetarian,
+    @required this.isLactoseFree,
+  });
+}
--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
+import '../dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
+
 //  final String id;
 //  final String title;
 //
@@ -13,12 +17,19 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final id = routeArgs['id'];
     final title = routeArgs['title'];
+
+    final categoryMeals =
+        DUMMY_MEALS.where((meal) => meal.categories.contains(id)).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Text('The Recipes for the category?'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
